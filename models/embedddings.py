d5b7099d1c4ce2179a01a7310880d6d693e90e36
babycar <zoya.v@ya.ru>
2023-03-10 11:23:57 +0300

    last emb version

--- models/embedddings.py
+++ models/embedddings.py
@@ -5,8 +5,16 @@ def random_embeddings(df, emb_size):
     inv_mapping = dict()
     users = list(set(df))
     for user in range(1, max(users) + 1):
-        new_vector = np.random.uniform(0, 1, size=emb_size)
-        #new_vector = np.ones(emb_size)
-        mapping[user] = new_vector
+       # print("aboba")
+        if emb_size==1:
+            new_vector = [user]
+            mapping[user] = np.asarray([user])
+        else:
+            new_vector = np.random.uniform(0, 1, size=emb_size)
+            mapping[user] = new_vector
         inv_mapping[tuple(new_vector)] = user
+    #print(mapping)
+    #exit()
     return mapping, inv_mapping
+
+# Function for calculating 2 digit
