f577fb8a0318f2878824e3770797e1fba712cecd
babycar <zoya.v@ya.ru>
2023-03-10 11:25:22 +0300

    some fix for discrete as reward

--- recsys_mdp/reward_functions.py
+++ recsys_mdp/reward_functions.py
@@ -7,10 +7,13 @@ def monotony_reward(df, key_mapping=None):
 def condition_reward(df, key_mapping=None):
     #TODO: condition shuld be customizable
     rewards = np.zeros(df.shape[0])
-    rewards[df[key_mapping['reward_col_name']] < 3] = 1
-    rewards[df[key_mapping['reward_col_name']] >= 3] = 0.1
+    rewards[df[key_mapping['reward_col_name']] <= 3] = 1
+    rewards[df[key_mapping['reward_col_name']] > 3] = -0.1
     return rewards
 
 def relevance_based_reward(df, key_mapping=None):
-    return df[key_mapping['reward_col_name']]
+    rewards = np.zeros(df.shape[0])
+    rewards[df[key_mapping['reward_col_name']] <= 3] = -1
+    rewards[df[key_mapping['reward_col_name']] > 3] = 1
+    return rewards
 
