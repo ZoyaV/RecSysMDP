0f232475a8b2e67c671e0b3f8c3de618c69d04c9
babycar <zoya.v@ya.ru>
2023-03-15 17:15:26 +0300

    fix

--- layers_experiment.sh
+++ layers_experiment.sh
@@ -10,11 +10,11 @@ python3 run_experiment.py  --config configs/SberZvuk_discrete_settings.yaml --ex
 
 
 
-python3 run_experiment.py  --config configs/SberZvuk_discrete_settings.yaml --experiment_name FreezeEmb_set_layers2 --framestack 10 --model_parametrs 1000 1000 16 32 10 128 1 0 &
+python3 run_experiment.py  --config configs/SberZvuk_discrete_settings.yaml --experiment_name FreezeEmb_set_layers2 --framestack 10 --model_parametrs 1000 1000 16 32 10 128 0 0 &
 
 python3 run_experiment.py  --config configs/SberZvuk_discrete_settings.yaml --experiment_name FreezeEmb_set_layers2 --framestack 10 --model_parametrs 1000 1000 16 32 10 128 1 32
 
 
-python3 run_experiment.py  --config configs/SberZvuk_discrete_settings.yaml --experiment_name FreezeEmb_set_layers2 --framestack 10 --model_parametrs 1000 1000 32 64 10 256 1 0 &
+python3 run_experiment.py  --config configs/SberZvuk_discrete_settings.yaml --experiment_name FreezeEmb_set_layers2 --framestack 10 --model_parametrs 1000 1000 32 64 10 256 0 0 &
 
 python3 run_experiment.py  --config configs/SberZvuk_discrete_settings.yaml --experiment_name FreezeEmb_set_layers2 --framestack 10 --model_parametrs 1000 1000 32 64 10 256 1 64
