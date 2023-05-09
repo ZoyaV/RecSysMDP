141ce0a60adcd85248daa38217196d8800018557
Petr Kuderov <pkuderov@gmail.com>
2023-05-09 23:04:13 +0300

    clean up web server

--- config_server.py
+++ config_server.py
@@ -1,29 +1,34 @@
+import multiprocessing
 import os
 import random
 import string
-import shutil
 import subprocess
-import multiprocessing
-from flask import Flask, request, render_template
+
 import yaml
+from flask import Flask, request, render_template
 
 app = Flask(__name__, template_folder='.')
-
 
 
 def run_experiment(config_file_path, group_name = "aboba", folder_name = None):
     print(group_name)
     # Создаем новый процесс
-    process = multiprocessing.Process(target=subprocess.call,
-                                      args=(["python3", "run_experiment.py",
-                                             "--config", config_file_path,
-                                             "--experiment_name", group_name,
-                                             "--folder_name", folder_name],))
+    process = multiprocessing.Process(
+        target=subprocess.call,
+        args=[
+            "python3", "run_experiment.py",
+            "--config", config_file_path,
+            "--experiment_name", group_name,
+            "--folder_name", folder_name
+        ]
+    )
     process.start()
+
 
 @app.route('/')
 def index():
     return render_template('experiment.html')
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -53,6 +58,7 @@ def upload_file():
     run_experiment(new_file_path, group_name = yaml_data['group_name'], folder_name = folder_name)
 
     return f'Aboba has been uploaded', 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
