ef783a47a4fda8eada8d300ea29d442b73e75687
babycar <zoya.v@ya.ru>
2023-05-13 09:55:25 +0300

    refactor data generator

--- generate_synth_data.py
+++ generate_synth_data.py
@@ -1,69 +1,35 @@
+import argparse
 import os
+import pickle
+from itertools import count
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
-from pathlib import Path
-from itertools import count
-
+import yaml
 from d3rlpy.base import LearnableBase
 
 from recsys_mdp.generators.scenarios.mdp_next_item_integration import NextItemEnvironment, TypesResolver
 from recsys_mdp.generators.utils.config import (
-    TConfig, GlobalConfig, LazyTypeResolver
+    GlobalConfig
 )
-import yaml
-import pickle
 
-def run(config_path = "recsys_mdp/generators/configs/mdp_next_item_integration.yaml"):
-    with open(config_path) as f:
-        config = yaml.load(f, Loader=yaml.Loader)
-    path_object = Path(config_path)
-    config_class = GlobalConfig(
-                config=config, config_path=path_object, type_resolver=TypesResolver()
-            )
 
-    env_conf = config['env']
-    env: NextItemEnvironment = config_class.resolve_object(
-        env_conf, object_type_or_factory=NextItemEnvironment
-    )
-
-    with open('env.pkl', 'wb') as f:
-        pickle.dump(env, f)
-        
-    model_conf = config['model']
-    model: LearnableBase = config_class.resolve_object(
-                model_conf | dict(use_gpu=False),
-                n_actions=env.n_items
-            )
-    gen_conf = config['generation']
-    return generate_dataset(gen_conf, env, model)
-
-def generate_dataset(gen_conf, env, model):
-        config = gen_conf
-        samples = []
-        for episode in count():
-            samples.extend(generate_episode(env, model))
-
-            if config['episodes_per_epoch'] is not None and episode >= config['episodes_per_epoch']:
-                break
-            if config['samples_per_epoch'] is not None and len(samples) >= config['samples_per_epoch']:
-                break
-        return samples
-def generate_episode(env, model):
+def generate_episode(env, model, framestack_size=10):
     env, model = env, model
     user_id = env.reset()
     trajectory = []
 
     # [10 last item_ids] + [user_id]
-    fake_obs = np.random.randint(0, 100, 10).tolist() + [user_id]
+    fake_obs = np.random.randint(0, env.n_items, framestack_size).tolist() + [user_id]
     obs = np.asarray(fake_obs)
 
     while True:
         try:
             item_id = model.predict(obs.reshape(1, -1))[0]
         except:
-            item_id = model.predict(obs[:10].reshape(1, -1))[0]
-        obs[:9] = obs[1:10]
+            item_id = model.predict(obs[:framestack_size].reshape(1, -1))[0]
+        obs[:framestack_size - 1] = obs[1:framestack_size]
         obs[-2] = item_id
 
         timestamp = env.timestamp
@@ -80,24 +46,75 @@ def generate_episode(env, model):
             break
     return trajectory
 
-if __name__ == "__main__":
-    trajectories = run()
+
+def generate_dataset(gen_conf, env, model):
+    config = gen_conf
+    samples = []
+    for episode in count():
+        samples.extend(generate_episode(env, model))
+        if config['episodes_per_epoch'] is not None and episode >= config['episodes_per_epoch']:
+            break
+        if config['samples_per_epoch'] is not None and len(samples) >= config['samples_per_epoch']:
+            break
+    return samples
+
+
+def make_env_setting(config_path="", env_name='default'):
+    # read configs
+    with open(config_path) as f:
+        config = yaml.load(f, Loader=yaml.Loader)
+    path_object = Path(config_path)
+    config_class = GlobalConfig(
+        config=config, config_path=path_object, type_resolver=TypesResolver()
+    )
+    # make conrollable environment
+    env_conf = config['env']
+    env: NextItemEnvironment = config_class.resolve_object(
+        env_conf, object_type_or_factory=NextItemEnvironment
+    )
+
+    # make model
+    model_conf = config['model']
+    model: LearnableBase = config_class.resolve_object(
+        model_conf | dict(use_gpu=False),
+        n_actions=env.n_items
+    )
+    gen_conf = config['generation']
+
+    # save environment object
+    with open(f'environments/{env_name}/env.pkl', 'wb') as f:
+        pickle.dump(env, f)
+
+    return gen_conf, env, model
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', dest='config')
+    parser.add_argument('--env_name', type=str)
+    args = parser.parse_args()
+
+    if args.config is None:
+        args.config = "recsys_mdp/generators/configs/mdp_next_item_integration.yaml"
+
+    directory = f'environments/{args.env_name}'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    gen_conf, env, model = make_env_setting(config_path=args.config, env_name=args.env_name)
+    trajectories = generate_dataset(gen_conf, env, model)
 
     # Define the column names
     column_names = ['timestamp', 'user_idx', 'item_idx', 'relevance_cont', 'relevance_int', 'terminated']
 
     # Create a DataFrame from the list of tuples
     df = pd.DataFrame(trajectories, columns=column_names)
-
-    # Create the "cases" directory if it does not exist
-    directory = 'row_data/cases'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    # Define the file name
-    case_name = 'case_name'
-    file_name = f"{directory}/{case_name}.csv"
+    file_name = f"{directory}/{args.env_name}.csv"
 
     # Save the DataFrame to a CSV file
     print("Data generated: ", df.shape)
     df.to_csv(file_name, index=False)
+
+
+if __name__ == "__main__":
+    main()
