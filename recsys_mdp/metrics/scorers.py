8d761a94abac3f4a383ba00871a954ebfa47434e
babycar <zoya.v@ya.ru>
2023-05-14 17:29:23 +0300

    add preference correlation

--- recsys_mdp/metrics/scorers.py
+++ recsys_mdp/metrics/scorers.py
@@ -1,10 +1,21 @@
 import numpy as np
 from recsys_mdp.metrics.metrics import ndcg, hit_rate
+from scipy.stats import pearsonr, spearmanr
+
 def log_covarage(total_prediction, users_interests, top_k):
-    distribution = np.asarray(total_prediction)
+    coverage_by_user = []
+    distribution = np.asarray(total_prediction[1])
     best_items = np.argmax(distribution, axis = 1)
+
+    for i in range(len(total_prediction[0])):
+        ratings = total_prediction[1][i]
+
+       # items = np.argmax(ratings)
+        #print(items)
+        cov = np.sum(ratings > 0.99)/len(total_prediction[1])
+        coverage_by_user.append(cov)
     #print(best_items)
-    return len(set(best_items))
+    return np.mean(coverage_by_user)
 
 def total_ndcg(total_prediction, users_interests, top_k):
     metricv = []
@@ -26,7 +37,7 @@ def interactive_hit_rates(interaction_result):
     hitrate = []
     for interact in interaction_result:
         interactive_items, original_items, \
-            users_interests, not_interactive_items = interact
+            users_interests, not_interactive_items, _, _ = interact
 
         interactive_hit_rate = hit_rate(interactive_items, original_items)
         interactive_hit_rate_full = hit_rate(interactive_items, users_interests)
@@ -34,6 +45,23 @@ def interactive_hit_rates(interaction_result):
         hitrate.append(interactive_hit_rate)
     return {'on_episode':np.mean(hitrate), 'on_history':np.mean(full_hitrate)}
 
+def preference_correlation(interaction_result):
+    mean_corr = []
+    for interact in interaction_result:
+        d1 = interact[-2] + 2
+        d2 = interact[-1]
+        d1 = np.asarray(d1)
+        d2 = np.asarray(d2)
+        idx = np.argsort(d1)
+
+      #  print(d1[idx], d2[idx])
+        pearson_corr, _ = pearsonr(d1[idx], d2[idx])
+
+      #  print(pearson_corr)
+        not_nan_mask = ~np.isnan(pearson_corr)
+        if  not_nan_mask:
+            mean_corr.append(pearson_corr)
+    return {'preference_correlation': np.mean(mean_corr)}
 def static_hit_rates(interaction_result):
     """
 
@@ -45,7 +73,7 @@ def static_hit_rates(interaction_result):
     hitrate = []
     for interact in interaction_result:
         interactive_items, original_items, \
-            users_interests, not_interactive_items = interact
+            users_interests, not_interactive_items, _, _ = interact
 
         interactive_hit_rate = hit_rate(not_interactive_items, original_items)
         interactive_hit_rate_full = hit_rate(not_interactive_items, users_interests)
